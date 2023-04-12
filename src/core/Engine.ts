36d3b7cd920d70243c9ba55d20ef097ea126ae6a
Aurorah Harmony <aurorahHarmony@gmail.com>
2023-04-12 22:52:38 +1200

    Render a triangle!

--- src/core/Engine.ts
+++ src/core/Engine.ts
@@ -7,6 +7,7 @@ import Shader from './gl/Shader';
 export default class Engine {
   private _canvas: HTMLCanvasElement;
   private _shader: Shader;
+  private _buffer: WebGLBuffer;
 
   /**
    * Create a new engine
@@ -25,6 +26,7 @@ export default class Engine {
 
     this.loadShaders();
     this._shader.use();
+    this.createBuffer();
 
     this.loop();
   }
@@ -36,13 +38,42 @@ export default class Engine {
     if (this._canvas !== undefined) {
       this._canvas.width = window.innerWidth;
       this._canvas.height = window.innerHeight;
+
+      gl.viewport(0, 0, this._canvas.width, this._canvas.height);
     }
   }
 
   private loop(): void {
     gl.clear(gl.COLOR_BUFFER_BIT); // Clear the color buffer.
 
+    gl.bindBuffer(gl.ARRAY_BUFFER, this._buffer);
+    gl.vertexAttribPointer(0, 3, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(0);
+
+    gl.drawArrays(gl.TRIANGLES, 0, 3);
+
     requestAnimationFrame(this.loop.bind(this));
+  }
+
+  private createBuffer(): void {
+    this._buffer = gl.createBuffer();
+
+    const vertices = [
+      // x,y,z
+      0, 0, 0,
+      //
+      0, 0.5, 0,
+      //
+      0.5, 0.5, 0,
+    ];
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, this._buffer);
+    gl.vertexAttribPointer(0, 3, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(0);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, undefined);
+    gl.disableVertexAttribArray(0);
   }
 
   private loadShaders(): void {
