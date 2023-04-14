63f7e1c6aa77494bd6cb67a9fcfe86db59eff220
Aurorah Harmony <aurorahHarmony@gmail.com>
2023-04-14 15:25:11 +1200

    Fix bug where the incorrect buffer type was being unbound

--- src/core/gl/GLBuffer.ts
+++ src/core/gl/GLBuffer.ts
@@ -106,7 +106,7 @@ export default class GLBuffer {
     for (const it of this._attributes) {
       gl.disableVertexAttribArray(it.location);
     }
-    gl.bindBuffer(gl.ARRAY_BUFFER, this._buffer);
+    gl.bindBuffer(this._targetBufferType, undefined);
   }
 
   /**
